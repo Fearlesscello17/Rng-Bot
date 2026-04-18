a6129ff1152d8d1878e746f16c257f91d4601a22
Fearlesscello17 <donthackmydiscordpls@gmail.com>
2026-04-18 03:13:12 +0200

    Add deploy-commands.js for Discord command registration

---
+++ deploy-commands.js
@@ -0,0 +1,31 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const commands = [
+  new SlashCommandBuilder().setName('roll').setDescription('Roll an aura'),
+  new SlashCommandBuilder().setName('coins').setDescription('Check coins'),
+  new SlashCommandBuilder().setName('shop').setDescription('View shop'),
+  new SlashCommandBuilder()
+    .setName('buy')
+    .setDescription('Buy item')
+    .addStringOption(option =>
+      option.setName('item')
+        .setDescription('Item name')
+        .setRequired(true))
+].map(cmd => cmd.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Registering commands...');
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log('Commands registered!');
+  } catch (error) {
+    console.error(error);
+  }
+})();
